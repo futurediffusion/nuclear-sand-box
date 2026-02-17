bf771db51d8d8f980db848fa2d29339b264ea7e4
willyzen6 <54221946+willyzen6@users.noreply.github.com>
2026-02-16 23:49:51 -0500

    HUD stamina fix + player adjustments

--- sripts/hud.gd
+++ sripts/hud.gd
@@ -1,25 +1,26 @@
 extends Control
 
-@onready var stamina_bar: TextureProgressBar = $StaminaBar
+# Apunta a StaminaBar2, que es la barra con texturas visuales
+@onready var stamina_bar: TextureProgressBar = $StaminaBar2
 
 var player: Player
 
 func _ready() -> void:
-	stamina_bar.min_value = 0.0
 	_try_bind_player()
 
 func _try_bind_player() -> void:
 	player = _find_player()
-	
+
 	if player == null:
-		# Intenta de nuevo el próximo frame
 		call_deferred("_try_bind_player")
 		return
 
-	# Set inicial
-	_update_bar(player.stamina, player.max_stamina)
+	# Configurar rango de la barra
+	stamina_bar.min_value = 0.0
+	stamina_bar.max_value = player.max_stamina
+	stamina_bar.value = player.stamina
 
-	# Conectar señal
+	# Conectar señal de stamina
 	if not player.stamina_changed.is_connected(_on_player_stamina_changed):
 		player.stamina_changed.connect(_on_player_stamina_changed)
 
@@ -27,12 +28,8 @@ func _find_player() -> Player:
 	var players := get_tree().get_nodes_in_group("player")
 	if players.size() > 0 and players[0] is Player:
 		return players[0] as Player
-	
 	return null
 
 func _on_player_stamina_changed(new_stamina: float, new_max_stamina: float) -> void:
-	_update_bar(new_stamina, new_max_stamina)
-
-func _update_bar(value: float, max_value: float) -> void:
-	stamina_bar.max_value = max_value
-	stamina_bar.value = value
+	stamina_bar.max_value = new_max_stamina
+	stamina_bar.value = new_stamina
